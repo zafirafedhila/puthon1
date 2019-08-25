a0871f320f660044c1785ada88cfa1670d9d7d8f
z <zafirafedhila@users.noreply.github.com>
2019-08-25 18:35:02 +0700

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,54 @@
+# for kondisi in array :
+#     code block
+# else :
+#     code block
+
+# for x in list
+# for Q in [0,1,2,3,4,5] :
+#     print(Q)
+
+# for Q in ['apel','mangga','sirsak','jambu'] :
+#     print(Q)
+
+# for x in range(mulai, akhir, loncat)
+# for x in range(0,10,2) :
+#     print(x)
+
+# for x in ['apel','mangga','sirsak','jambu'] :
+#     if x == 'sirsak':
+#         pass
+#     else :
+#         print(x)
+#     print('ye -', x)
+
+# for x in ['apel','mangga','sirsak','jambu'] :
+#     if x == 'sirsak':
+#         continue
+#     else :
+#         print(x)
+
+#     print('ye -', x)
+
+for x in ['apel','mangga','sirsak','jambu'] :
+    if x == 'sirsak':
+        break
+    else :
+        print(x)
+
+    print('ye -', x)
+
+# x = 0
+# while x < 10 :
+#     print(x,'***')
+#     x += 2
+
+
+# x = 0
+# while x < 5 :
+#     print(x,'*')
+#     print(x,'**')
+#     print(x,'***')
+#     print(x,'**')
+#     print(x,'*')
+
+#     x += 5
